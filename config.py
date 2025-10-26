34a7f324576a3b6a9439e2e28b44a32984d0dcc3
Timur <45330851+timur8889@users.noreply.github.com>
2025-10-27 00:18:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    BOT_TOKEN = "8313764660:AAEOFtGphxmLLz7JKSa82a179-vTvjBu1lo"
+    BOT_TOKEN = os.getenv('BOT_TOKEN', 'YOUR_BOT_TOKEN_HERE')
     HITMOS_API_URL = "https://hitmos.me/api"
-    WEBHOOK_URL = "https://your-domain.com"
-    WEBAPP_HOST = "0.0.0.0"
+    WEBHOOK_URL = os.getenv('WEBHOOK_URL', 'https://your-domain.com')
+    WEBAPP_HOST = os.getenv('WEBAPP_HOST', '0.0.0.0')
     WEBAPP_PORT = int(os.environ.get('PORT', 5000))
