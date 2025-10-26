338b03a14e152e57a2600cb359e587c34f4529dc
Timur <45330851+timur8889@users.noreply.github.com>
2025-10-27 00:13:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    BOT_TOKEN = "YOUR_BOT_TOKEN"
+    BOT_TOKEN = "8313764660:AAEOFtGphxmLLz7JKSa82a179-vTvjBu1lo"
     HITMOS_API_URL = "https://hitmos.me/api"
     WEBHOOK_URL = "https://your-domain.com"
     WEBAPP_HOST = "0.0.0.0"
